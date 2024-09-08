6458b52254276ee144fdad15a39d778151aab009
ziad <ziad.nouh111@gmail.com>
2024-09-08 13:39:21 +0300

    feat: provide the routes with the contexts

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Route, Router } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { HomePage } from "./pages/HomePage";
 import { PostDetailsPage } from "./pages/PostDetailsPage";
+import { AuthProvider } from "./contexts/AuthContext";
+import { PostProvider } from "./contexts/PostContext";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Route path="/posts" Component={HomePage} />
-      <Route path="/posts/:id" Component={PostDetailsPage} />
-    </Router>
+    <AuthProvider>
+      <PostProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/posts/:id" element={<PostDetailsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </PostProvider>
+    </AuthProvider>
   );
 }
 
