f172f0abe47f54db302877c12b2f021a7488609c
ziad <ziad.nouh111@gmail.com>
2024-09-10 16:59:17 +0300

    feat: add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const { user, isLoggedIn, login, logout } = useAuthContext();
+
+  return (
+    <header className="px-4 sticky top-0 z-50 bg-slate-600">
+      <div className="navbar ">
+        <div className="navbar-start ">
+          <Link to="/" className="btn ">
+            Home
+          </Link>
+        </div>
+        <div></div>
+        <div className="navbar-end">
+          {isLoggedIn ? (
+            <>
+              <button onClick={logout} className="btn btn-error">
+                Logout
+              </button>
+            </>
+          ) : (
+            <Link to={"/login"}>
+              <button onClick={login} className="btn btn-primary">
+                Login
+              </button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
