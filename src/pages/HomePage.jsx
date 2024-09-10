c576583e3a73b76b3c9463e1d2259cc524dcc476
ziad <ziad.nouh111@gmail.com>
2024-09-10 17:02:09 +0300

    feat: add posts and user data to HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { usePostContext } from "../contexts/PostContext";
+import { PostCard } from "../components/PostCard";
+import { PlusCircle } from "@phosphor-icons/react";
+import AddEditPostModal from "../components/AddEditPostModal";
+import { useAuthContext } from "../contexts/AuthContext";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const { posts, fetchPosts } = usePostContext();
+  const { openAddPostModal } = usePostContext();
+  const { isLoggedIn } = useAuthContext();
+
+  useEffect(() => {
+    fetchPosts();
+  }, [posts]);
+
+  return (
+    <div className="flex justify-center relative ">
+      <div className=" max-w-[20rem] container mx-7 md:max-w-[30rem] lg:max-w-[40rem]">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+
+      {isLoggedIn && (
+        <div
+          className="fixed bottom-5 right-5 cursor-pointer"
+          onClick={openAddPostModal}
+        >
+          <PlusCircle size={48} color="#21c47e" weight="bold" />
+        </div>
+      )}
+
+      <AddEditPostModal />
+    </div>
+  );
 };
